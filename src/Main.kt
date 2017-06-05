e387b9efdf6d8d241c49171bdc61c3975bad06f9
Alex Oladele <dragid10@gmail.com>
2017-06-05 14:42:17 -0400

    Implement loading of config file

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,6 @@
 import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
 import java.util.*
 import kotlin.concurrent.timerTask
 
@@ -10,33 +12,68 @@ import kotlin.concurrent.timerTask
  */
 
 fun main(args: Array<String>) {
-//    Username and Password for DB
-    val dbInfo = File("DBUserName_Password.txt").readLines()
 
-//    Create SQL object with username and password
-    val mySQL = MySQL(dbInfo[0], dbInfo[1])
+    val prop: Properties = Properties()
+    val input: InputStream
+
+    try {
+//    Load the Properties from the config file
+        input = FileInputStream("config.prop")
+        prop.load(input)
+        input.close()
+    } catch (e: Exception) {
+        e.printStackTrace()
+        // TODO Add fallback actions
+    } finally {
+//        Create the property objects for the different classes
+        val dbProperties = createDBProperties(prop)
+        val twitterProperties = createTwitterProperties(prop)
+
+        //    Create SQL object with username and password
+        val mySQL = MySQL(dbProperties)
 
 //    Create Twitter object to interact with Twitter
-    val twitterObj = Twitter2()
-
-//    Connects to the DB
-//    mySQL.connectToMySQL()
+        val twitterObj = Twitter2(twitterProperties)
 
 //    Connects to Twitter
-    val twitterConn = twitterObj.connectToTwitter()
+        val twitterConn = twitterObj.connectToTwitter()
 
-    //    Adds lyrics to DB
+        //    Adds lyrics to DB
 //    val lyricsFiles = File("lyrics").listFiles()
 //    addDirectoryToDB(mySQL, *lyricsFiles)
 
-    val timer = Timer()
-    val hourlyTask = timerTask({
-        mySQL.connectToMySQL()
-        twitterObj.tweetLyrics(mySQL.getRandomLyric()!!, twitterConn)
-        mySQL.closeConnection()
-    })
+        val timer = Timer()
+        val hourlyTask = timerTask({
+            mySQL.connectToMySQL()
+            twitterObj.tweetLyrics(mySQL.getRandomLyric()!!, twitterConn)
+            mySQL.closeConnection()
+            println("Database Connection successfully closed")
+        })
+        timer.schedule(hourlyTask, 0L, ((1000 * 60 * 60) * 2))
+    }
+}
 
-    timer.schedule(hourlyTask, 0L, ((1000 * 60 * 60) * 2))
+fun createTwitterProperties(prop: Properties): Properties {
+    val twitterProp = Properties()
+    twitterProp.setProperty("debug", prop.getProperty("debug"))
+    twitterProp.setProperty("oauth.consumerKey", prop.getProperty("oauth.consumerKey"))
+    twitterProp.setProperty("oauth.consumerSecret", prop.getProperty("oauth.consumerSecret"))
+    twitterProp.setProperty("oauth.accessToken", prop.getProperty("oauth.accessToken"))
+    twitterProp.setProperty("oauth.accessTokenSecret", prop.getProperty("oauth.accessTokenSecret"))
+
+    return twitterProp
+}
+
+fun createDBProperties(prop: Properties): Properties {
+    val dbProp = Properties()
+    dbProp.setProperty("user", prop.getProperty("db_user"))
+    dbProp.setProperty("password", prop.getProperty("db_pass"))
+    dbProp.setProperty("autoReconnect", prop.getProperty("autoReconnect"))
+    dbProp.setProperty("userSSL", "false")
+    dbProp.setProperty("jdbc_Driver", prop.getProperty("jdbc_Driver"))
+    dbProp.setProperty("dbURL", prop.getProperty("dbURL"))
+
+    return dbProp
 }
 
 fun addDirectoryToDB(mySQL: MySQL, vararg songList: File) {
@@ -90,3 +127,4 @@ fun addSongToDB(song: File, mySQLContext: MySQL) {
             .forEach { mySQLContext.addLyricsToDB(it) }
     println("Lyrics Added to Database!")
 }
+
